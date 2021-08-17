c77934d2f517e008a17e3ca217b46e28d8c19b5a
Rafael Mian <rafaelmian35@gmail.com>
2021-08-16 22:18:57 -0300

    asd

--- frontend/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import HeaderNavBar from "./Header/HeaderNavBar";
-import ListItems from "./Header/ListItems";
-
-const Header = () => {
-  return <HeaderNavBar children={<ListItems />} />;
-};
-
-export default Header;
--- frontend/src/components/Hero/Video.js
+++
@@ -1,8 +0,0 @@
-const Video = () => (
-  <video autoPlay="autoplay" loop="loop" muted id="video-id" className="video">
-    <source src="./assets/video.mp4" type="video/mp4" />
-    Your browser does not support the video tag.
-  </video>
-);
-
-export default Video;
