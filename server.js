186d4c3b339dfd1e877bcd55c1bf0811c8f34edf
Rafael Mian <rafaelmian35@gmail.com>
2021-09-03 05:46:23 -0300

    host

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 require("./config/passport");
 require("./config/database");
+const path = require("path");
 
 const router = require("./routes/index");
 const adminRouter = require("./routes/adminRoutes");
@@ -14,6 +15,13 @@ app.use(cors());
 
 app.use("/api", router);
 app.use("/admin", adminRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("/client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+}
 app.listen(process.env.PORT || 4000, process.env.HOST || "0.0.0.0", () =>
   console.log("Listening on port 4000")
 );
