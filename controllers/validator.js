77b5562bcbd8cf3a65f6fe7981242c4008ece085
Rafael Mian <rafaelmian35@gmail.com>
2021-09-03 14:25:54 -0300

    patterns

--- controllers/validator.js
+++ controllers/validator.js
@@ -5,7 +5,11 @@ const validator = (req, res, next) => {
       .string()
       .trim()
       .min(2)
-      .pattern(new RegExp("^[a-zA-ZÀ-ÿ]*$"))
+      .pattern(
+        new RegExp(
+          "/^[a-zA-Z\u00f1\u00d1]+(s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/g"
+        )
+      )
       // .pattern(
       //   new RegExp(
       //     "/^[a-zA-Z\u00f1\u00d1]+(s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/g"
@@ -14,20 +18,25 @@ const validator = (req, res, next) => {
       .required()
       .messages({
         "string.empty": "First name field must be completed",
-        "string.pattern.base": "Name field must not contain numbers",
-        "string.min": "Name must be at least 2 characters long",
+        "string.pattern.base": "First name field must not contain numbers",
+        "string.min": "First name must be at least 2 characters long",
       }),
     last_name: joi
       .string()
       .trim()
       .min(2)
       // .pattern(new RegExp("^[a-zA-Z]*$"))
-      .pattern(new RegExp("^[a-zA-ZÀ-ÿ]*$"))
+      .pattern(
+        new RegExp(
+          "/^[a-zA-Z\u00f1\u00d1]+(s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/g"
+        )
+      )
+      // .pattern(new RegExp("^[a-zA-ZÀ-ÿ]*$"))
       .required()
       .messages({
         "string.empty": "Last name field must be completed",
-        "string.pattern.base": "Name field must not contain numbers",
-        "string.min": "Name must be at least 2 characters long",
+        "string.pattern.base": "Last name field must not contain numbers",
+        "string.min": "Last name must be at least 2 characters long",
       }),
     email: joi.string().trim().required().email().messages({
       "string.empty": "Email field must be completed",
