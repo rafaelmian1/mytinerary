1b8a59ab6913727589dd9b58240ac692a89d55d7
Rafael Mian <rafaelmian@MacBook-Pro-de-Rafael.local>
2023-02-16 01:46:07 -0300

    build

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const token = localStorage.getItem('token')
-const baseURL = 'http://localhost:3002/api'
+const baseURL = 'http://34.201.18.25:3002/api'
 const headers = { Authorization: `Bearer ${token}` }
 
 export default axios.create({ baseURL, headers })
